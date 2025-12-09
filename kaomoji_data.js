438d668db48ac0f93b2b3228e3acd75db8007098
Ian-Woo-elice <symphonie913@gmail.com>
2025-12-09 18:53:07 +0900

    Add files via upload

---
+++ kaomoji_data.js
@@ -0,0 +1,660 @@
+window.KAOMOJI_DATA = 
+[
+  {
+    "emoji": "(^_^)",
+    "description": "미소",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "smile",
+      "happy",
+      "basic"
+    ]
+  },
+  {
+    "emoji": "(^o^)",
+    "description": "활짝",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "laugh",
+      "joy",
+      "big"
+    ]
+  },
+  {
+    "emoji": "(*^▽^*)",
+    "description": "기쁨",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "happy",
+      "excited",
+      "joy"
+    ]
+  },
+  {
+    "emoji": "(≧∇≦)/",
+    "description": "신남/손흔듦",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "excited",
+      "high",
+      "hello"
+    ]
+  },
+  {
+    "emoji": "(❁´◡`❁)",
+    "description": "꽃미소",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "flower",
+      "soft",
+      "happy"
+    ]
+  },
+  {
+    "emoji": "╰(*°▽°*)╯",
+    "description": "만세/환호",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "hooray",
+      "hands",
+      "celebrate"
+    ]
+  },
+  {
+    "emoji": "o(*￣▽￣*)o",
+    "description": "만족",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "satisfied",
+      "relax",
+      "good"
+    ]
+  },
+  {
+    "emoji": "( ´ ▽ ` )ﾉ",
+    "description": "안녕",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "hello",
+      "wave",
+      "greeting"
+    ]
+  },
+  {
+    "emoji": "(●'◡'●)",
+    "description": "방긋",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "smile",
+      "cute",
+      "simple"
+    ]
+  },
+  {
+    "emoji": "٩(◕‿◕)۶",
+    "description": "귀여움",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "cute",
+      "happy",
+      "cheer"
+    ]
+  },
+  {
+    "emoji": "(o^∀^o)",
+    "description": "헤헤",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "laugh",
+      "joy"
+    ]
+  },
+  {
+    "emoji": "(*￣3￣)╭",
+    "description": "룰루랄라",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "whistle",
+      "happy",
+      "relax"
+    ]
+  },
+  {
+    "emoji": "φ(゜▽゜*)♪",
+    "description": "음표/노래",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "music",
+      "singing",
+      "happy"
+    ]
+  },
+  {
+    "emoji": "o(≧∀≦)o",
+    "description": "완전 신남",
+    "category": "기쁨/행복 (^_^)",
+    "tags": [
+      "super",
+      "excited",
+      "yeah"
+    ]
+  },
+  {
+    "emoji": "(♥_♥)",
+    "description": "하트 눈",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "heart",
+      "love",
+      "eyes"
+    ]
+  },
+  {
+    "emoji": "(´▽`ʃ♡ƪ)",
+    "description": "사랑해",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "love",
+      "heart",
+      "give"
+    ]
+  },
+  {
+    "emoji": "(づ￣ 3￣)づ",
+    "description": "뽀뽀/츄",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "kiss",
+      "love",
+      "chu"
+    ]
+  },
+  {
+    "emoji": "♡( ◡‿◡ )",
+    "description": "수줍은 사랑",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "love",
+      "shy",
+      "soft"
+    ]
+  },
+  {
+    "emoji": "( *︾▽︾)",
+    "description": "부끄",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "shy",
+      "blush",
+      "love"
+    ]
+  },
+  {
+    "emoji": "(｡♥‿♥｡)",
+    "description": "러블리",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "lovely",
+      "heart",
+      "cute"
+    ]
+  },
+  {
+    "emoji": "♥(ˆ⌣ˆԅ)",
+    "description": "쓰담쓰담",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "pat",
+      "comfort",
+      "love"
+    ]
+  },
+  {
+    "emoji": "(✿ ♥‿♥)",
+    "description": "꽃하트",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "flower",
+      "love",
+      "pretty"
+    ]
+  },
+  {
+    "emoji": "(*￣3￣)╭♡",
+    "description": "하트 날리기",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "kiss",
+      "fly",
+      "heart"
+    ]
+  },
+  {
+    "emoji": "(❤ ω ❤)",
+    "description": "반함",
+    "category": "사랑/애정 (♥)",
+    "tags": [
+      "crush",
+      "love",
+      "sparkle"
+    ]
+  },
+  {
+    "emoji": "(T_T)",
+    "description": "울음",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "cry",
+      "sad",
+      "tears"
+    ]
+  },
+  {
+    "emoji": "(┬┬﹏┬┬)",
+    "description": "오열",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "cry",
+      "sobbing",
+      "sad"
+    ]
+  },
+  {
+    "emoji": "(._.)",
+    "description": "시무룩",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "sad",
+      "down",
+      "sorry"
+    ]
+  },
+  {
+    "emoji": "(;´༎ຶД༎ຶ`)",
+    "description": "통곡",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "cry",
+      "loud",
+      "pain"
+    ]
+  },
+  {
+    "emoji": "ಥ_ಥ",
+    "description": "눈물 뚝뚝",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "cry",
+      "tears",
+      "sad"
+    ]
+  },
+  {
+    "emoji": "(ToT)",
+    "description": "으앙",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "cry",
+      "sad",
+      "upset"
+    ]
+  },
+  {
+    "emoji": "::>_<::",
+    "description": "찡그림",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "cry",
+      "sad",
+      "hurt"
+    ]
+  },
+  {
+    "emoji": "(ok...)",
+    "description": "체념",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "giveup",
+      "sad",
+      "ok"
+    ]
+  },
+  {
+    "emoji": "(+_+)d",
+    "description": "기절/아픔",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "dizzy",
+      "sick",
+      "hurt"
+    ]
+  },
+  {
+    "emoji": "(´。＿。｀)",
+    "description": "우울",
+    "category": "슬픔/눈물 (T_T)",
+    "tags": [
+      "gloomy",
+      "sad",
+      "down"
+    ]
+  },
+  {
+    "emoji": "(╯°□°）╯︵ ┻━┻",
+    "description": "밥상 뒤집기",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "table flip",
+      "angry",
+      "rage"
+    ]
+  },
+  {
+    "emoji": "-_-",
+    "description": "정색",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "serious",
+      "blank",
+      "speechless"
+    ]
+  },
+  {
+    "emoji": "(o_O)?",
+    "description": "궁금/물음표",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "confused",
+      "question",
+      "what"
+    ]
+  },
+  {
+    "emoji": "¯\\_(ツ)_/¯",
+    "description": "몰라/어깨으쓱",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "shrug",
+      "dunno",
+      "whatever"
+    ]
+  },
+  {
+    "emoji": "(╬▔皿▔)╯",
+    "description": "분노",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "angry",
+      "rage",
+      "mad"
+    ]
+  },
+  {
+    "emoji": "o((>ω< ))o",
+    "description": "버둥버둥/패닉",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "panic",
+      "struggle",
+      "help"
+    ]
+  },
+  {
+    "emoji": "(⊙_⊙;)",
+    "description": "당황/땀",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "shock",
+      "sweat",
+      "confused"
+    ]
+  },
+  {
+    "emoji": "(;ŏ﹏ŏ)",
+    "description": "진땀",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "sweat",
+      "nervous",
+      "worry"
+    ]
+  },
+  {
+    "emoji": "(￣y▽,￣)╭ ",
+    "description": "거만",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "arrogant",
+      "hmm"
+    ]
+  },
+  {
+    "emoji": "凸(￣ヘ￣)",
+    "description": "흥/삐짐",
+    "category": "화남/당황 (-_-)",
+    "tags": [
+      "hmph",
+      "ignore",
+      "angry"
+    ]
+  },
+  {
+    "emoji": "m(_ _)m",
+    "description": "배꼽인사/사과",
+    "category": "인사/제스처 (Bye)",
+    "tags": [
+      "bow",
+      "sorry",
+      "hello"
+    ]
+  },
+  {
+    "emoji": "( ﾟдﾟ)つ Bye",
+    "description": "작별",
+    "category": "인사/제스처 (Bye)",
+    "tags": [
+      "bye",
+      "wave",
+      "see ya"
+    ]
+  },
+  {
+    "emoji": "( *・ω・)✄╰ひ╯",
+    "description": "손절(?)/자름",
+    "category": "인사/제스처 (Bye)",
+    "tags": [
+      "cut",
+      "scissor"
+    ]
+  },
+  {
+    "emoji": "(*￣▽￣)d",
+    "description": "따봉/최고",
+    "category": "인사/제스처 (Bye)",
+    "tags": [
+      "thumb",
+      "good",
+      "great"
+    ]
+  },
+  {
+    "emoji": "( •̀ ω •́ )✧",
+    "description": "자신감/반짝",
+    "category": "인사/제스처 (Bye)",
+    "tags": [
+      "confident",
+      "sparkle",
+      "wink"
+    ]
+  },
+  {
+    "emoji": "(>'-'<)",
+    "description": "화이팅",
+    "category": "인사/제스처 (Bye)",
+    "tags": [
+      "cheer",
+      "fighting",
+      "support"
+    ]
+  },
+  {
+    "emoji": "o(￣┰￣*)ゞ",
+    "description": "충성/경례",
+    "category": "인사/제스처 (Bye)",
+    "tags": [
+      "salute",
+      "yes",
+      "sir"
+    ]
+  },
+  {
+    "emoji": "(=^・^=)",
+    "description": "고양이",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "cat",
+      "meow",
+      "neko"
+    ]
+  },
+  {
+    "emoji": "ʕ•ᴥ•ʔ",
+    "description": "곰",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "bear",
+      "cute",
+      "animal"
+    ]
+  },
+  {
+    "emoji": "(U・x・U)",
+    "description": "강아지",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "dog",
+      "puppy",
+      "bark"
+    ]
+  },
+  {
+    "emoji": "o( =•ω•= )m",
+    "description": "고양이 인사",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "cat",
+      "hello",
+      "cute"
+    ]
+  },
+  {
+    "emoji": "V(=^･ω･^=)v",
+    "description": "브이 고양이",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "cat",
+      "v",
+      "peace"
+    ]
+  },
+  {
+    "emoji": "ᶘ ᵒᴥᵒᶅ",
+    "description": "물개/아기곰",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "seal",
+      "bear",
+      "cute"
+    ]
+  },
+  {
+    "emoji": "⊂(￣(工)￣)⊃",
+    "description": "큰 곰",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "bear",
+      "big",
+      "hug"
+    ]
+  },
+  {
+    "emoji": "/ᐠ｡ꞈ｡ᐟ\\",
+    "description": "고양이 얼굴",
+    "category": "동물 친구들 ʕ•ᴥ•ʔ",
+    "tags": [
+      "cat",
+      "face",
+      "cute"
+    ]
+  },
+  {
+    "emoji": "(¦ꒉ[▓▓]",
+    "description": "수면/이불",
+    "category": "행동/상태 (Run)",
+    "tags": [
+      "sleep",
+      "bed",
+      "tired"
+    ]
+  },
+  {
+    "emoji": "ε=ε=ε=(~￣▽￣)~",
+    "description": "달리기/퇴근",
+    "category": "행동/상태 (Run)",
+    "tags": [
+      "run",
+      "dash",
+      "home"
+    ]
+  },
+  {
+    "emoji": "...φ(︶▽︶)φ...",
+    "description": "메모/공부",
+    "category": "행동/상태 (Run)",
+    "tags": [
+      "write",
+      "study",
+      "memo"
+    ]
+  },
+  {
+    "emoji": "┬┴┬┴┤(･_├┬┴┬┴",
+    "description": "빼꼼/지켜보고있다",
+    "category": "행동/상태 (Run)",
+    "tags": [
+      "peek",
+      "wall",
+      "watch"
+    ]
+  },
+  {
+    "emoji": "(/≧▽≦)/",
+    "description": "점프/환호",
+    "category": "행동/상태 (Run)",
+    "tags": [
+      "jump",
+      "high",
+      "yay"
+    ]
+  },
+  {
+    "emoji": "┗( T﹏T )┛",
+    "description": "항복/도망",
+    "category": "행동/상태 (Run)",
+    "tags": [
+      "surrender",
+      "run",
+      "help"
+    ]
+  },
+  {
+    "emoji": "┌( ಠ_ಠ)┘",
+    "description": "뚜벅뚜벅",
+    "category": "행동/상태 (Run)",
+    "tags": [
+      "walk",
+      "go",
+      "move"
+    ]
+  }
+];
